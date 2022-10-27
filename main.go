409362a867f43889ce0c2659ed1f6bcb78807262
348134071@qq.com <348134071@qq.com>
2022-10-27 17:14:40 +0800

    .

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"container_manager/client"
+	"container_manager/route"
+	"github.com/gin-gonic/gin"
 	"log"
 )
 
@@ -11,11 +13,11 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	//r := gin.Default()
-	//route.Route(r)
-	//
-	//err = r.Run(":8000")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
+	r := gin.Default()
+	route.Route(r)
+
+	err = r.Run(":8000")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
