0191b0a9a98b0d2f677143030675b26a6e2cae87
348134071@qq.com <348134071@qq.com>
2022-10-28 10:32:27 +0800

    .

--- route/router.go
+++ route/router.go
@@ -10,7 +10,7 @@ func Route(r *gin.Engine) {
 	{
 		gi.POST("create", controller.NewInferControl().Create)
 		gi.POST("extend_expiry", controller.NewInferControl().ExtendExpiry)
-		gi.GET("get", controller.NewInferControl().Create)
+		gi.GET("get", controller.NewInferControl().Get)
 	}
 
 }
